073b64b3850e9b32ee7e11489b69acdad68de432
Heric Breno Da Silva <119009016+sheric2223@users.noreply.github.com>
2023-03-29 16:05:07 -0300

    criando form
    criando formulário responsivo

---
+++ sricpt.js
@@ -0,0 +1,52 @@
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const jobSelectec = document.querySelector("#message")
+const menssageTextarea = document.querySelector("#message")
+
+form.addEventListener("submit",(event) => {
+    event.preventDefault();
+    if(nameInput.value ==="") {
+        alert("Campo De Nome Está vazio")
+        return;
+    }
+
+    if(nameInput.value ==="" || !emailValid(emailInput.value, 8)){
+        alert("Campo De Email Está Vazio");
+        return;
+    }
+    if(!validatePassword.value){
+        alert("A senha precisa ser no minimo 8 digitos");
+        return;
+    }
+
+    if(jobSelectec.value ===""){
+        alert("Selecione A Sua Situação");
+        return;
+    } 
+
+    if(menssageTextarea.value ===""){
+        alert("Não Deixe O campo Vazio");
+        return;
+    } 
+
+    form.submit();
+});
+
+function emailValid(email){
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    );
+    if(emailRegex.test(email)){
+        return true;
+    }else
+        return false;
+    }
+
+    function validatePassword(password, minDigits) {
+        if(password.length >=minDigits)
+        return true
+
+        return false
+    }
